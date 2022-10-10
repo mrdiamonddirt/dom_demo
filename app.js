182bfc80ec593905ae5227deef8911e118cfca5b
RowDog <mrdiamonddirt@gmail.com>
2022-10-10 12:06:01 +0100

    button makes images disappear

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 //in javascript
-const img = document.getElementById('img');
-const change_image = function() {
-  if (img.dataset.image == "user1") {
-  	img.src = "/images/small.gif";
-    img.dataset.image = "user2";
-    return
-  };
-  
-  if (img.dataset.image == "user2") {
-  	img.src = "/images/user1.jpg";
-    img.dataset.image = "user1";
-    return
-  };
+var imageDisplayed = true;
+const img = document.getElementById("img");
+
+const change_image = function () {
+  if (img.dataset.image == "logo1") {
+    img.src = "/images/small.gif";
+    img.dataset.image = "logo2";
+    return;
+  }
+  if (img.dataset.image == "logo2") {
+    img.src = "/images/discord.webp";
+    img.dataset.image = "logo1";
+    return;
+  }
 };
+
+document.getElementById("myBtn").addEventListener("click", function () {
+  // alert("Print");
+  var img = document.getElementById("img");
+
+  if (imageDisplayed == true) {
+    // var img = document.getElementById("img");
+    img.style.display = "none";
+    imageDisplayed = false;
+    console.log(imageDisplayed);
+    return;
+  } else {
+    // var img = document.getElementById("img");
+    img.style.display = "block";
+    imageDisplayed = true;
+    console.log(imageDisplayed);
+    return;
+  }
+});
