babd0daa4274157c992883805b6a2bd603ab0bd7
RowDog <mrdiamonddirt@gmail.com>
2022-10-10 11:43:42 +0100

    renamed files

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const Image = document.getElementById('logo');
-const input = document ;
-
-document.addEventListener('click', (event) => {
-    console.log(event);
-})
+//in javascript
+const img = document.getElementById('img');
+const change_image = function() {
+  if (img.dataset.image == "user1") {
+  	img.src = "/images/small.gif";
+    img.dataset.image = "user2";
+    return
+  };
+  
+  if (img.dataset.image == "user2") {
+  	img.src = "/images/user1.jpg";
+    img.dataset.image = "user1";
+    return
+  };
+};
